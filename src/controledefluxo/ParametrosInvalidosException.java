d36f48e3cafdad50654fddf3ea8d980e71677a6b
wandersonxc <wandersonxcarvalho@gmail.com>
2023-11-20 17:49:19 -0300

    PROJETO FINALIZADO - DESAFIO ( CONTROLE DE FLUXO )

---
+++ src/controledefluxo/ParametrosInvalidosException.java
@@ -0,0 +1,8 @@
+package controledefluxo;
+
+@SuppressWarnings("serial")
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
